832cc3833e36de05abab1e89a7924f3087ebb47b
El-ad David Amir <elad@custora.com>
2013-08-22 16:38:27 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "El-ad David Amir"
+    email     "elad@custora.com"
+    password  "password"
+    password_confirmation "password"
+  end
+end
